10233135930c5bc9be9197cbcd555f55057ec6f1
David Parker <davidparker@flywheel.io>
2023-04-12 10:48:37 -0400

    refactor filetype detection

--- fw_gear_file_validator/parser.py
+++ fw_gear_file_validator/parser.py
@@ -46,6 +46,33 @@ def parse_config(
     return debug, tag, schema_file_path, fw_ref, loader_config
 
 
+def check_mimetype(input_file: Union[dict, Path, str]):
+    if isinstance(input_file, dict):
+        mime = input_file["object"]["mimetype"]
+        input_file = input_file.get("location", {}).get("name")
+        return mime, input_file
+    return None, input_file
+
+
+def check_ext(input_file: Union[Path, str]):
+    if isinstance(input_file, str):
+        input_file = Path(input_file)
+    # If it's a path try to get the extension from the suffix
+    return input_file.suffix
+
+def validate_filetype(ext: str, mime: str):
+    """ Ensures detected filetype is supported and errors if not"""
+    input_file_type = None
+    if ext:
+        input_file_type = SUPPORTED_FILE_EXTENSIONS.get(ext)
+    elif mime:
+        input_file_type = SUPPORTED_FLYWHEEL_MIMETYPES.get(mime)
+    if input_file_type is None:
+        raise TypeError(f"file type {mime},{ext} is not supported")
+
+    return input_file_type
+
+
 def identify_file_type(input_file: Union[dict, str, Path]) -> str:
     """Given a flywheel config input file object, identify the file type."""
     ext = None
@@ -55,23 +82,9 @@ def identify_file_type(input_file: Union[dict, str, Path]) -> str:
     if not input_file:
         return ""
 
-    if isinstance(input_file, dict):
-        # First try to just check the file type from the file extension:
-        mime = input_file["object"]["mimetype"]  # If it's a string make it a path
-        input_file = input_file.get("location", {}).get("name")
-
-    if isinstance(input_file, str):
-        input_file = Path(input_file)
-    # If it's a path try to get the extension from the suffix
-    ext = input_file.suffix
-
-    # If we managed to get an extension, that means that
-    if ext:
-        input_file_type = SUPPORTED_FILE_EXTENSIONS.get(ext)
-    elif mime:
-        input_file_type = SUPPORTED_FLYWHEEL_MIMETYPES.get(mime)
-    if input_file_type is None:
-        raise TypeError(f"file type {mime},{ext} is not supported")
+    mime, input_file = check_mimetype(input_file)
+    ext = check_ext(input_file)
+    input_file_type = validate_filetype(ext, mime)
 
     return input_file_type
 
