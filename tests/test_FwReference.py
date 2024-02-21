4f244cad192f949e0b2b05bacd98870cae012b3b
David Parker <davidparker@flywheel.io>
2024-02-21 15:08:01 -0500

    fixed test_FwReference.py

--- tests/test_FwReference.py
+++ tests/test_FwReference.py
@@ -25,31 +25,11 @@ def test_is_valid():
     assert ref.path_is_valid()
 
     with pytest.raises(ValueError):
-        _ = FwReference(type="none", parents={})
-
-    for parent in PARENT_ORDER:
-        ref = FwReference(type=parent, parents={})
-        assert ref.path_is_valid()
+        _ = FwReference.init_from_file(None, {"label": "123"})
 
 
 def test_parents():
     ses_id = "63ceeda12bae5aafaf66306e"
-    ses_label = "test_ses"
-    client = MagicMock()
-    parents = {
-        "group": "gid",
-        "project": "pid",
-        "subject": "sid",
-        "session": None,
-        "acquisition": None,
-    }
-    session = Session(id=ses_id, label=ses_label, parents=parents)
-
-    ref = FwReference.init_from_object(client, session)
-    _ = ref.fw_object()
-
-    client.get_session.assert_called_once()
-    client.get_session().get_file.assert_not_called()
 
     client = MagicMock()
     parents = {"session": ses_id}
@@ -58,10 +38,10 @@ def test_parents():
     file_type = "json"
 
     file = FileEntry(name=file_name, file_id=file_id, type=file_type, parents=parents)
-
-    ref = FwReference.init_from_object(client, file)
+    client.get_file.return_value = file
+    ref = FwReference.init_from_file(client, file)
     _ = ref.hierarchy_objects
-    client.get_file.assert_called_once()
+    assert client.get_file.call_count == 2
     client.get_session.assert_called_once()
 
 
@@ -98,8 +78,10 @@ def test_get_lookup_path():
         "acquisition": acquisition,
         "file": file,
     }
+    mock_client = MagicMock()
+    mock_client.get_file.return_value = file
 
-    fw_ref = FwReference.init_from_object(None, file)
+    fw_ref = FwReference.init_from_file(mock_client, file)
     fw_ref.hierarchy_objects = object_hierarchy
     url = f"fw://{group.label}/{project.label}/{subject.label}/{session.label}/{acquisition.label}/{file.name}"
     assert fw_ref.get_lookup_path() == url
@@ -120,57 +102,4 @@ def test_get_lookup_path():
     assert fw_ref.get_lookup_path(level="group") == url
 
 
-def test_container_retrys():
-    utils.SLEEP_TIME = 0.1
 
-    my_client = MagicMock()
-    my_client.get_file = MagicMock(return_value=None)
-    file_name = "test_file_name.ext"
-    file_id = "test_container_id"
-    file_type = "test_file_type"
-    parents = {}
-
-    file = FileEntry(name=file_name, file_id=file_id, type=file_type, parents=parents)
-
-    fw_ref = FwReference.init_from_object(my_client, file)
-
-    with pytest.raises(ValueError) as e_info:
-        fw_ref.fw_object
-
-    assert my_client.get_file.call_count == utils.N_TRIES
-
-
-def test_parent_retrys():
-    utils.SLEEP_TIME = 0.1
-
-    session_label = "test_session"
-    my_client = MagicMock()
-    session = Session()
-    session.label = session_label
-    session.parents = {
-        "group": "gid",
-        "project": "pid",
-        "subject": "sid",
-        "session": None,
-        "acquisition": None,
-    }
-
-    my_client.get_session = MagicMock(return_value=session)
-    my_client.get_subject = MagicMock(return_value=None)
-    my_client.get_project = MagicMock(return_value=None)
-    my_client.get_group = MagicMock(return_value=None)
-
-    fw_ref = FwReference(
-        id="test_container_id",
-        type="session",
-        name=session_label,
-        is_file=False,
-        _client=my_client,
-        input_object=session,
-        parents=session.parents,
-    )
-
-    with pytest.raises(ValueError) as e_info:
-        fw_ref.hierarchy_objects
-
-    assert my_client.get_group.call_count == utils.N_TRIES
