bacc0f253895e2d215dfff2ca85f838c988bbaf1
David Parker <davidparker@flywheel.io>
2024-04-01 14:42:58 -0400

    added tests

--- tests/test_caster.py
+++ tests/test_caster.py
@@ -3,11 +3,13 @@ import pytest
 
 JSON_TYPES = {"string": str, "number": float, "integer": int, "boolean": bool, "null": utils.null}
 
+
 def test_cast_one_valid():
     cast_type = int
     value = "123"
     new_value = utils.cast_one(value, cast_type)
     assert new_value == int(value)
+
 
 def test_cast_one_invalid():
     cast_type = int
@@ -15,11 +17,13 @@ def test_cast_one_invalid():
     new_value = utils.cast_one(value, cast_type)
     assert new_value == value
 
+
 def test_cast_types_single_valid():
     cast_type = [int]
     value = "123"
     new_value = utils.cast_csv_val(value, cast_type)
     assert new_value == int(value)
+
 
 def test_cast_types_single_invalid():
     cast_type = [int]
@@ -27,11 +31,13 @@ def test_cast_types_single_invalid():
     new_value = utils.cast_csv_val(value, cast_type)
     assert new_value == value
 
+
 def test_cast_types_single():
     cast_type = [int]
     value = "123"
     new_value = utils.cast_csv_val(value, cast_type)
     assert new_value == int(value)
+
 
 def test_cast_null_valid():
     cast_type = [utils.null, str]
@@ -39,25 +45,27 @@ def test_cast_null_valid():
     new_value = utils.cast_csv_val(value, cast_type)
     assert new_value is None
 
+
 def test_cast_null_invalid():
     cast_type = [utils.null, str]
     value = 'abc'
     new_value = utils.cast_csv_val(value, cast_type)
     assert new_value == value
 
+
 def test_cast_multiple_invalid():
     cast_type = [int, str]
     value = '123'
     with pytest.raises(ValueError):
-        new_value = utils.cast_csv_val(value, cast_type)
+        _ = utils.cast_csv_val(value, cast_type)
 
     cast_type = [int, str]
     value = 123
     with pytest.raises(ValueError):
-        new_value = utils.cast_csv_val(value, cast_type)
+        _ = utils.cast_csv_val(value, cast_type)
+
 
 def test_cast_multiple_valid():
-
     cast_type = [int, str]
     value = 'abc'
     new_value = utils.cast_csv_val(value, cast_type)
